c79f679488f9f3453957e2c561073709635398d0
maximkovalev <mkovalevhse@yandex.ru>
2022-07-22 14:08:35 +0300

    init

--- App.js
+++ App.js
@@ -1,11 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Image,
+  ImageBackground,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
 
-export default function App() {
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar translucent barStyle="light-content" />
+      <ImageBackground
+        source={require("./assets/home-intro.gif")}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <View
+          style={{
+            position: "absolute",
+            backgroundColor: "rgba(0,0,0,0.65)",
+            width: "100%",
+            height: "100%",
+          }}
+        ></View>
+        <View style={styles.logo}>
+          <Image
+            source={require("./assets/logo.png")}
+            style={{ width: 90, height: 90, marginBottom: 100 }}
+          />
+        </View>
+        <View style={{ margin: 40 }}>
+          <TextInput
+            style={{
+              color: "#ffffff",
+              borderWidth: 1,
+              borderColor: "#fff",
+              height: 50,
+              width: "100%",
+              borderRadius: 10,
+              paddingLeft: 20,
+              paddingRight: 20,
+            }}
+            placeholder="Email..."
+            placeholderTextColor="#fff"
+            onChangeText={(email) => setEmail(email)}
+          />
+          <TextInput
+            style={{
+              color: "#ffffff",
+              borderWidth: 1,
+              borderColor: "#fff",
+              height: 50,
+              width: "100%",
+              marginTop: 20,
+              borderRadius: 10,
+              paddingLeft: 20,
+              paddingRight: 20,
+            }}
+            placeholder="Password..."
+            secureTextEntry={true}
+            placeholderTextColor="#fff"
+            onChangeText={(email) => setEmail(email)}
+          />
+          <TouchableOpacity style={styles.loginBtn}>
+            <Text style={{ color: "white" }}>Войти</Text>
+          </TouchableOpacity>
+          <Button title="Войти с помощью Apple ID" color="#fff" />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +83,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(224,173,173,0.75)",
+  },
+  loginBtn: {
+    width: "100%",
+    borderRadius: 10,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "tomato",
+  },
+  logo: {
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
   },
 });
+
+export default App;
